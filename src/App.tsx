ccad485942f2126d119132df146b2485678782a8
Juan Velasquez <juanvelasquez@Juans-MacBook-Air.local>
2023-02-28 23:28:48 -0500

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from "solid-js";
+
+import "./App.css";
+
+const App: Component = () => {
+  return (
+    <div class="App">
+      <main>Tiktak</main>
+    </div>
+  );
+};
+
+export default App;
